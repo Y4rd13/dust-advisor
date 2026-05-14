9eb1317cde3d89a3463ece09714152a8e17d1536
Y4rd13 <benjaserrau@gmail.com>
2026-05-13 21:12:42 -0400

    feat(algorithm): add core domain types (Rarity, Premium, CardMeta, CollectionEntry, AdvisorOptions, DustPlan)

---
+++ src/DustAdvisor.Algorithm/Domain/CardMeta.cs
@@ -0,0 +1,22 @@
+namespace DustAdvisor.Algorithm.Domain
+{
+    public sealed class CardMeta
+    {
+        public string CardId { get; }
+        public int DbfId { get; }
+        public string Name { get; }
+        public Rarity Rarity { get; }
+        public CardSet Set { get; }
+        public bool IsCollectible { get; }
+
+        public CardMeta(string cardId, int dbfId, string name, Rarity rarity, CardSet set, bool isCollectible)
+        {
+            CardId = cardId;
+            DbfId = dbfId;
+            Name = name;
+            Rarity = rarity;
+            Set = set;
+            IsCollectible = isCollectible;
+        }
+    }
+}
---
+++ src/DustAdvisor.Algorithm/Domain/DustItem.cs
@@ -0,0 +1,28 @@
+namespace DustAdvisor.Algorithm.Domain
+{
+    public sealed class DustItem
+    {
+        public string CardId { get; }
+        public string CardName { get; }
+        public Rarity Rarity { get; }
+        public int RegularToDust { get; }
+        public int GoldenToDust { get; }
+        public int DustGained { get; }
+        public bool InRefundWindow { get; }
+        public bool IsStandardLegal { get; }
+
+        public DustItem(string cardId, string cardName, Rarity rarity,
+                        int regularToDust, int goldenToDust, int dustGained,
+                        bool inRefundWindow, bool isStandardLegal)
+        {
+            CardId = cardId;
+            CardName = cardName;
+            Rarity = rarity;
+            RegularToDust = regularToDust;
+            GoldenToDust = goldenToDust;
+            DustGained = dustGained;
+            InRefundWindow = inRefundWindow;
+            IsStandardLegal = isStandardLegal;
+        }
+    }
+}
